080436ce63306d99e7313984a030005fac9d6deb
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-02-12 20:37:27 +0530

    code

--- python.py
+++ python.py
@@ -1,12 +1,10 @@
-"""num2 = int(input("enter some number : "))
-if num2 >= 1:
-    print("positive number ")
-elif num2 == 0:
-    print("Zero")
-elif num2 < 0:
-    print("negative number ")"""
+
 
 i = 1
-while i <= 100:
+while i <=100:
+    if i % 2 == 1:
+        i += 1
+        continue
     print(i)
-    i +=1
+    i += 1
+
