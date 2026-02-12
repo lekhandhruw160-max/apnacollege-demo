a392fc4fc22efa04b63edc5364db8a1dbabe0881
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-02-12 20:26:06 +0530

    code

--- python.py
+++ python.py
@@ -1,7 +1,12 @@
-num2 = int(input("enter some number : "))
+"""num2 = int(input("enter some number : "))
 if num2 >= 1:
     print("positive number ")
 elif num2 == 0:
     print("Zero")
 elif num2 < 0:
-    print("negative number ")
+    print("negative number ")"""
+
+i = 1
+while i <= 100:
+    print(i)
+    i +=1
